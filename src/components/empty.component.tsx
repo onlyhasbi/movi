198d2c9dbc9b60e01184cd13abaf161156905d4a
Muhammad Hasbi <hasbi@akasia.id>
2024-06-22 13:35:38 +0800

    bugfix type empty component

--- src/components/empty.component.tsx
+++ src/components/empty.component.tsx
@@ -2,7 +2,7 @@ import NoMovie from "../assets/no-movie.svg?react";
 
 type Props = {
   title?: string;
-  variant: "info" | "notfound";
+  variant?: "info" | "notfound";
 };
 
 const Empty = ({ title, variant = "info" }: Props) => {
@@ -22,7 +22,7 @@ const Empty = ({ title, variant = "info" }: Props) => {
           }}
         >
           <NoMovie
-            style={{ width: "50rem", height: "50srem", marginBottom: "2rem" }}
+            style={{ width: "50rem", marginBottom: "2rem" }}
           />
           <p style={style}>Movie Not Found</p>
         </div>
